d4683ed9c2467f25fb9bdee15f1e506a8fd4472c
Floyd Orr <floydcanhelp@gmail.com>
2019-03-25 17:16:03 -0500

    add constructor and var names

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var bag = '../img/bag.jpg';
+var banana = '../img/banana.jpg';
+var bathroom = '../img/bathroom';
+var boots ='../img/boots.jpg';
+var breakfast = '../img/breakfast.jpg';
+var boots = '../img/bublegum.jpg';
+var bubblegum = '../img/bubblegum.jpg';
+var chair = '../img/chair.jpg';
+var cthulhu = '../img/cthulhu.jpg';
+var dogduck = '../img/dog-duck.jpg';
+var dragon = '../img/dragon.jpg';
+var pen = '../img/pen.jpg';
+var petsweep = '../img/petsweep.jpg';
+var scissors = '../img/scissors.jpg';
+var shark = '../img/shark.jpg';
+var sweep = '../img/sweep.jpg';
+var tauntaun = '../img/tauntaun.jpg';
+var unicorn = '../img/unicorn.jpg';
+var usb = '../img/usb.jpg';
+var watercan = '../img/usb.jpg';
+var wineglass = '../img/wineglass.jpg';
+
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
