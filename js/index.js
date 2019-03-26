d093d2809a13a427081894a748934b8fde581988
Glitch (bus-mall) <none>
2019-03-26 20:09:56 +0000

    🏈👠 Updated with Glitch

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
+var totalClickGlobal = 1;
+var data = [
+  { imageName: 'bag', path: '../img/bag.jpg', },
+  { imageName: 'banana', path: '../img/banana.jpg', },
+  { imageName: 'bathroom', path: '../img/bathroom.jpg', },
+  { imageName: 'boots', path: '../img/boots.jpg', },
+  { imageName: 'breakfast', path: '../img/breakfast.jpg', },
+  { imageName: 'bubblegum', path: '../img/bubblegum.jpg', },
+  { imageName: 'chair', path: '../img/chair.jpg', },
+  { imageName: 'cthulhu', path: '../img/cthulhu.jpg', },
+  { imageName: 'dogduck', path: '../img/dogduck.jpg', },
+  { imageName: 'dragon', path: '../img/dragon.jpg', },
+  { imageName: 'pen', path: '../img/pen.jpg', },
+  { imageName: 'petsweep', path: '../img/petsweep.jpg', },
+  { imageName: 'scissors', path: '../img/scissors.jpg', },
+  { imageName: 'shark', path: '../img/shark.jpg', },
+  { imageName: 'sweep', path: '../img/sweep.jpg', },
+  { imageName: 'tauntaun', path: '../img/tauntaun.jpg', },
+  { imageName: 'unicorn', path: '../img/unicorn.jpg', },
+  { imageName: 'usb', path: '../img/usb.jpg', },
+  { imageName: 'watercan', path: '../img/watercan.jpg', },
+  { imageName: 'wineglass', path: '../img/wineglass.jpg', }
+];
 
- var totalClickGlobal = 0;
 var shown = {
   'bag': 0, 'banana': 0, 'bathroom' : 0, 'boots': 0, 'breakfast': 0, 'bubblegum': 0, 'chair': 0, 'cthulhu': 0,' dogduck': 0,'dragon': 0, 'pen': 0,'petsweep': 0, 'scissors': 0,'shark': 0,'sweep': 0, 'tauntaun': 0,'unicorn': 0,'usb': 0,
   'watercan': 0,'wineglass': 0
@@ -7,76 +29,72 @@ var shown = {
 var allImages = ['../img/bag.jpg', '../img/banana.jpg','../img/bathroom.jpg','../img/boots.jpg','../img/breakfast.jpg','../img/bubblegum.jpg', '../img/chair.jpg','../img/cthulhu.jpg', '../img/dogduck.jpg', '../img/dragon.jpg','../img/pen.jpg','../img/petsweep.jpg','../img/scissors.jpg','../img/shark.jpg', '../img/sweep.jpg','../img/tauntaun.jpg', '../img/unicorn.jpg','../img/usb.jpg','../img/watercan.jpg','../img/wineglass.jpg'];
 var allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu',' dogduck','dragon', 'pen','petsweep', 'scissors','shark','sweep', 'tauntaun','unicorn','usb','watercan','wineglass'];
 
-function makeRandom() {
-  var rand1 = Math.floor(Math.random() * allImages.length);
-  return rand1;
-}
+const makeRandom = () => Math.floor(Math.random() * allImages.length);
 
-inital(makeRandom(), makeRandom(), makeRandom());
-function inital(picone, pictwo,picthree) {
-  var picOne = `<img src='${allImages[picone]}' width='100%' height='auto' data-pic="${allNames[picone]}">`;
-  var num = allNames[picone];
-  shown[num]++;
-  
-  var picTwo = `<img src='${allImages[pictwo]}' width='100%' height='auto' data-pic="${allNames[pictwo]}">`;
-  var num2 = allNames[pictwo];
-  shown[num2]++;
-
-  var picThree = `<img src='${allImages[picthree]}' width='100%' height='auto' data-pic="${allNames[picthree]}">`;
-  var num3 = allNames[picthree];
-  shown[num3]++;
+function inital(rand1, rand2, rand3) {
+  var picOne = `<img src='${data[rand1].path}' width='100%' height='auto' data-pic="${data[rand1].imageName}">`;
+  var picTwo = `<img src='${data[rand2].path}' width='100%' height='auto' data-pic="${data[rand2].imageName}">`;
+  var picThree = `<img src='${data[rand3].path}' width='100%' height='auto' data-pic="${data[rand3].imageName}">`;
 
   // HTMl Selectors
   var picLeft = document.getElementById('picLeft');
   var picMiddle= document.getElementById('picMiddle');
-  var picLast = document.getElementById('picLast');
+  var picRight = document.getElementById('picRight');
 
   picLeft.innerHTML = picOne;
   picMiddle.innerHTML = picTwo;
-  picLast.innerHTML = picThree;
+  picRight.innerHTML = picThree;
 }
 
-function BusMall(img, name) {
-  // this.img = `<img src='${img}' width='100%' height='auto' onclick='pic.count(this)' data-pic-one="${name}">`;
-  this.name = name;
+inital(makeRandom(), makeRandom(), makeRandom());
+
+function BusMall(img) {
+  this.name = img.imageName;
+  this.path = img.path;
   this.totalClick = 0;
-  this.totalView = 0;
-  this.nameAndTotal = 0;
-  this.totalPerentage = 0;
-  this.count = function(img) {
-    var imgClick = img.getAttribute('data-pic');
-    // this.totalClick += 1;
-    this.totalView = 0; //this pic has been shown this many times
-    this.nameAndTotal += 1; //this pic clickd this many times
-    totalClickGlobal += 1;///at 25 show 
-    // console.log(totalClickGlobal, ' totalClickGlobal' );
-    // experemntal 
-    BusMall.all[i].nameAndTotal++;
-
-  };
+  this.totalViews = 0;
+  this.clickPercentage = 0;
 }
+
 BusMall.all = [];
 
-for (var i = 0; i < allNames.length; i++ ) {
-  BusMall.all.push(new BusMall(allImages[i], allNames[i]));
+for (var i = 0; i < data.length; i++ ) {
+  BusMall.all.push(new BusMall(data[i]));
 }
 
 var picLeft = document.getElementById('picLeft').addEventListener('click', handleClick);
 var picMiddle= document.getElementById('picMiddle').addEventListener('click', handleClick);
-var picLast = document.getElementById('picLast').addEventListener('click', handleClick);
+var picRight = document.getElementById('picRight').addEventListener('click', handleClick);
 
-function handleClick(event){
-  for(var i = 0; i < BusMall.all.length; i++){
-    if(event.target.getAttribute('data-pic') === BusMall.all[i].name){
-      BusMall.all[i].totalClick++;
-      /////exprermental
-      BusMall.all[i].totalClick++;
+function handleClick(event) {
+  let shownImages = document.querySelectorAll('[data-pic]').forEach(e => {
+    BusMall.all.find(function(img) {
+      if (img.name === e.getAttribute('data-pic')) {
+        img.totalViews++;
+      }
+    });
+  });
 
-      break;
+  let img = event.target.getAttribute('data-pic');
+  for(var i = 0; i < BusMall.all.length; i++) {
+
+    if (BusMall.all[i].name === img) {
+      BusMall.all[i].totalClick++;
+    }
+
+    if (BusMall.all[i].totalClick === BusMall.all[i].totalViews) {
+      BusMall.all[i].clickPercentage = 100;
+    } else {
+      BusMall.all[i].clickPercentage = (BusMall.all[i].totalClick / BusMall.all[i].totalViews) * 100;
     }
   }
-  inital(makeRandom(), makeRandom(), makeRandom());
-}
 
-console.table(BusMall.all, ' buss mall all 112');
-//console.log(shown, ' shown');
+  if (totalClickGlobal < 5) {
+    totalClickGlobal += 1;
+    inital(makeRandom(), makeRandom(), makeRandom());
+  } else {
+    document.querySelector('#stats').innerHTML = `<pre>${JSON.stringify(BusMall.all, null, 3)}</pre>`;
+  }
+
+  console.log('BusMall', BusMall.all);
+}
